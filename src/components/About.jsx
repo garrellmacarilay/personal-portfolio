4006ce73767901c1c920fbbf96d565f970c20e5c
GarrellMac <gmacarilay1@gmail.com>
2025-10-13 07:03:55 +0800

    Change the Font and Edit the About Component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,10 +7,7 @@ function About() {
       <div className="container section-title" data-aos="fade-up">
         <h2>About</h2>
         <p>
-          Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex
-          aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos
-          quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia
-          fugiat sit in iste officiis commodi quidem hic quas.
+          I’m a dream-driven individual with a bold vision for the future — one that fuels my determination, discipline, and relentless hard work. As a 3rd-year student, I’m in the grind, turning challenges into stepping stones and lessons into growth. I believe that failure is not the opposite of success, but an essential part of it. Every setback strengthens my resolve and brings me closer to the life I’m working to build. My journey is far from over, but every day, I’m moving forward — one step, one effort, one dream at a time.
         </p>
       </div>
       {/* End Section Title */}
@@ -24,7 +21,7 @@ function About() {
             />
           </div>
           <div className="col-lg-8 content">
-            <h2>UI/UX Designer &amp; Web Developer.</h2>
+            <h2>Developer &amp; Web3 Developer.</h2>
             <p className="fst-italic py-3">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -34,19 +31,19 @@ function About() {
                 <ul>
                   <li>
                     <i className="bi bi-chevron-right" />{" "}
-                    <strong>Birthday:</strong> <span>1 May 1995</span>
+                    <strong>Name:</strong> <span>Garrell B. Macarilay</span>
                   </li>
                   <li>
                     <i className="bi bi-chevron-right" />{" "}
-                    <strong>Website:</strong> <span>www.example.com</span>
-                  </li>
-                  <li>
-                    <i className="bi bi-chevron-right" />{" "}
-                    <strong>Phone:</strong> <span>+123 456 7890</span>
+                    <strong>Phone:</strong> <span>+63 9195947844</span>
                   </li>
                   <li>
                     <i className="bi bi-chevron-right" /> <strong>City:</strong>{" "}
-                    <span>New York, USA</span>
+                    <span>Marikina, Philippines</span>
+                  </li>
+                  <li>
+                    <i className="bi bi-chevron-right" /> <strong>Motto:</strong>{" "}
+                    <span>Keep Moving Forward</span>
                   </li>
                 </ul>
               </div>
@@ -54,30 +51,23 @@ function About() {
                 <ul>
                   <li>
                     <i className="bi bi-chevron-right" /> <strong>Age:</strong>{" "}
-                    <span>30</span>
+                    <span>21</span>
                   </li>
                   <li>
                     <i className="bi bi-chevron-right" />{" "}
-                    <strong>Degree:</strong> <span>Master</span>
+                    <strong>Course:</strong> <span>BS Information System</span>
                   </li>
                   <li>
                     <i className="bi bi-chevron-right" />{" "}
-                    <strong>Email:</strong> <span>email@example.com</span>
+                    <strong>Email:</strong> <span>gmacarilay1@gmail.com</span>
                   </li>
                   <li>
                     <i className="bi bi-chevron-right" />{" "}
-                    <strong>Freelance:</strong> <span>Available</span>
+                    <strong>Hobbies:</strong> <span>Guitar, Drawing, Cycling, Workout</span>
                   </li>
                 </ul>
               </div>
             </div>
-            <p className="py-3">
-              Officiis eligendi itaque labore et dolorum mollitia officiis optio
-              vero. Quisquam sunt adipisci omnis et ut. Nulla accusantium dolor
-              incidunt officia tempore. Et eius omnis. Cupiditate ut dicta
-              maxime officiis quidem quia. Sed et consectetur qui quia
-              repellendus itaque neque.
-            </p>
           </div>
         </div>
       </div>
